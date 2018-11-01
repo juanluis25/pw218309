2b5fd144d656e2d74c58b6b81a190b8da98b0864
juanluis25 <42280394+juanluis25@users.noreply.github.com>
2018-11-01 10:01:16 -0700

    JavaScript
    casi terminado!!

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,7 +1,48 @@
-
-		var colorAmarillo = function(x){
-			x.style.background= "yellow"
+window.onload=function(){
+		var colorAmarillo = function(){
+			this.style.background= "yellow"
 		}
-		var colorBlanco = function(x){
-			x.style.background= "white"
+		var colorBlanco = function(){
+			this.style.background= "white"
  }
+ var operando1 = document.getElementById("operando1");
+ operando1.addEventListener("focus", colorAmarillo);
+ operando1.addEventListener("focusout", colorBlanco);
+
+ var operador=""
+ var numeros=function(){
+ 	var valor=this.value;
+ 	if (operador==""){
+ 	var valorInput=document.getElementById("operando1").value;
+ 	if(valorInput=="0"){
+ 		document.getElementById("operando1")value="";
+ 		}
+ 		document.getElementById("operando1").value+=valor;
+			
+ 		}else{ 
+ 		if (operador==""){
+ 	var valorInput=document.getElement("operando2").value;
+ 	if(valorInput=="0"){
+ 		document.getElementById("operando2")value="";
+ 		}
+ 		document.getElementById("operando2").value+=valor;		
+ 	}
+ 	var uno=document.getElementById("uno")
+ 	uno.addEventListener("click", numeros)
+ 	 	var dos=document.getElementById("dos")
+ 	dos.addEventListener("click", numeros)
+ 	 	var tres=document.getElementById("tres")
+ 	tres.addEventListener("click", numeros)
+ 	 	var cuatro=document.getElementById("cuatro")
+ 	cuatro.addEventListener("click", numeros)
+ 	 	var cinco=document.getElementById("cinco")
+ 	cinco.addEventListener("click", numeros)
+ 	 	var seis=document.getElementById("seis")
+ 	seis.addEventListener("click", numeros)
+ 	 	var siete=document.getElementById("siete")
+ 	siete.addEventListener("click", numeros)
+ 	 	var ocho=document.getElementById("ocho")
+ 	ocho.addEventListener("click", numeros)
+ 	 	var nueve=document.getElementById("nueve")
+ 	nueve.addEventListener("click", numeros)
+}
